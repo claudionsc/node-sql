02502b4903c1a1530fdbed10ff7339618e2c068a
alvigabriel <claudio.alves20112011@hotmail.com>
2023-01-15 11:41:04 -0300

    migrations, dotenv

--- src/database/migrations/20230115141325-Users.js
+++ src/database/migrations/20230115141325-Users.js
@@ -6,17 +6,40 @@ module.exports = {
     /**
      * Add altering commands here.
      *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+     * Example: */
+      await queryInterface.createTable('users', { 
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        email: {
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false
+        },
+        updated_at: {
+          type: Sequelize.DATE,
+          allowNull: false
+        },
+      });
+     
   },
 
   async down (queryInterface, Sequelize) {
     /**
      * Add reverting commands here.
      *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+     * Example: */
+      await queryInterface.dropTable('users');
+     
   }
 };
